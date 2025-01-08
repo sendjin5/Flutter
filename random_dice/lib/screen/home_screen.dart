311fa8d2b2c65e59486d03f73eb591557f116560
sendjin5 <sendjin5@gmail.com>
2025-01-08 11:35:38 +0900

    practice

--- random_dice/lib/screen/home_screen.dart
+++ random_dice/lib/screen/home_screen.dart
@@ -6,7 +6,10 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() => 
+  _HomeScreenState(),
+  _HomeStateAdd();
+
 }
 
 class _HomeScreenState extends State<HomeScreen> {
@@ -27,6 +30,21 @@ class _HomeScreenState extends State<HomeScreen> {
             },
             child: Text('랜덤한 숫자의 값 : $number'),
           ),
+        ));
+  }
+}
+
+class _HomeStateAdd extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Container(
+              child: Text(
+            '이 내용은 뭐죠?',
+            style: TextStyle(fontSize: 50, color: Colors.amber),
+          )),
         ));
   }
 }
